09e958f20b6816e305693299c16c561ada1111db
GwenP88 <12777@holbertonstudents.com>
2025-12-10 19:43:42 +0100

    add modif

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,34 +6,33 @@
  * in ascending order using the Bubble sort algorithm
  * @array: array
  * @size: size of array
- * Return:
+ * Return:nothing, void function
  */
 
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int temp;
-int swapped = 0;
+	size_t i, j;
+	int temp;
+	int swapped = 0;
 
-if (array == NULL || size < 2)
-return;
+	if (array == NULL || size < 2)
+	return;
 
-for (i = 0; i < size - 1; i++)
-{
-	for (j = 0; j < size - i - 1; j++)
+	for (i = 0; i < size - 1; i++)
 	{
-		if (array[j] > array[j + 1])
+		for (j = 0; j < size - i - 1; j++)
 		{
-		temp = array[j];
-		array[j] = array[j + 1];
-		array[j + 1] = temp;
-		swapped = 1;
-		print_array(array, size);
+			if (array[j] > array[j + 1])
+			{
+			temp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+			swapped = 1;
+			print_array(array, size);
+			}
 		}
+		if (swapped == 0)
+		break;
 	}
-	if (swapped == 0)
-	break;
-}
-
 }
 
