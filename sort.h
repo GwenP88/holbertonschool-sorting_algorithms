3f2b56d77ea2d0717ac2aba0cf151472b47cfdf7
GwenP88 <12777@holbertonstudents.com>
2025-12-11 11:49:30 +0100

    add file 0-O

--- sort.h
+++ sort.h
@@ -25,9 +25,9 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
-void bubble_sort(int *array, size_t size); /*Task 0*/
-void insertion_sort_list(listint_t **list); /*Task 1*/
-void selection_sort(int *array, size_t size); /*Task 2*/
-void quick_sort(int *array, size_t size); /*Task 3*/
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
